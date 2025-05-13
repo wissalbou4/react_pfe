5792f294d55f793a12f1a7e7750f5ec7591d114e
wissal-github <bouazaouiwissal@gmail.com>
2025-05-13 22:55:17 +0100

    commit2

--- src/components/Dashboard/MedcinDashboard.jsx
+++ src/components/Dashboard/MedcinDashboard.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import ConsulteRendezVous from '../../Documents/ListeRendezvous';
 
-const MedcinDashboard = ({ activeComponent }) => {
- 
+const MedcinDashboard = ({ activeComponent,setActiveComponent  }) => {
+  useEffect(() => {
+    setActiveComponent('rendez_vous');
+  }, [setActiveComponent]);
 return (
   <div>
     
   <div
   className=" min-h-screen p-6"
   style={{
-    backgroundImage:` url('bg-banner.jpg'`,
+  
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
